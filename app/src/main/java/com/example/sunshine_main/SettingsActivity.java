098b53f6c2afcb44c0fdb3b123e97463528e1452
Firdosh Ansari <firdosh.ansari0202@gmail.com>
2022-04-11 22:20:24 +0530

    initial commit

---
+++ app/src/main/java/com/example/sunshine_main/SettingsActivity.java
@@ -0,0 +1,28 @@
+package com.example.sunshine_main;
+
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
